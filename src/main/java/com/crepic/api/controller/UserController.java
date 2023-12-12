e45617302c945d066237a480e5447ef932e3e2cc
Juan Felipe Cerquera Arboleda <juan.cerquera.dev@gmail.com>
2023-12-12 13:13:17 -0500

    arreglos servicios

--- src/main/java/com/crepic/api/controller/UserController.java
+++ src/main/java/com/crepic/api/controller/UserController.java
@@ -1,14 +1,12 @@
 package com.crepic.api.controller;
 
 import com.crepic.api.dto.BaseEntity;
-import com.crepic.api.dto.BaseResponse;
 import com.crepic.api.model.User;
 import com.crepic.api.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.client.HttpServerErrorException;
 
 @RestController
 @RequestMapping("user")
--- src/main/java/com/crepic/api/dto/BaseResponse.java
+++
@@ -1,6 +0,0 @@
-package com.crepic.api.dto;
-
-import org.springframework.http.HttpEntity;
-
-public class BaseResponse <T>{
-}
